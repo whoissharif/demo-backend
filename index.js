3399f9badf5417c10a49de1f8fe538f154ffa4de
whoissharif <sharif.csebubt@gmail.com>
2021-11-30 16:55:53 +0600

    sending response

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded());
 
-
+app.get('/admin', (req, res)=> {
+    res.send('admin page');
+});
 
 app.listen(4000, () => {
     console.log('server is running');
